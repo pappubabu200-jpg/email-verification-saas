813b8345f77bb7dc41dfed393b0d98cb6756fa1b
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 18:41:09 +0530

    Update webhook_endpoint.py

--- Backend/app/schemas/webhook_endpoint.py
+++ Backend/app/schemas/webhook_endpoint.py
@@ -1,1 +1,15 @@
+from .base import ORMBase
 
+
+class WebhookEndpointResponse(ORMBase):
+    user_id: int | None
+    team_id: int | None
+    url: str
+    description: str | None
+    secret: str
+    events: str
+    active: bool
+    api_version: str | None
+    last_success_at: str | None
+    last_failure_at: str | None
+    last_status_code: int | None
