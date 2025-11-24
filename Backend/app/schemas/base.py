95b58747abb88bc1a91b901ce9c5019287ce74f0
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 17:51:00 +0530

    Update base.py

--- Backend/app/schemas/base.py
+++ Backend/app/schemas/base.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+from pydantic import BaseModel
 
+
+class ORMBase(BaseModel):
+    id: int
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    class Config:
+        from_attributes = True
