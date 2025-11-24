e4c4fe26456d1334ba159da8897782c8bdeb7b3b
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 17:59:12 +0530

    Update failed_job.py

--- Backend/app/schemas/failed_job.py
+++ Backend/app/schemas/failed_job.py
@@ -1,1 +1,9 @@
+from .base import ORMBase
+from pydantic import BaseModel
 
+
+class FailedJobResponse(ORMBase):
+    job_type: str
+    job_id: int | None
+    attempts: int
+    error_message: str
