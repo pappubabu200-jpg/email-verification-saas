d729ba34b2d908d003c0d626bd87c80616e00d32
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 17:56:26 +0530

    Update subscription.py

--- Backend/app/schemas/subscription.py
+++ Backend/app/schemas/subscription.py
@@ -1,1 +1,15 @@
+from .base import ORMBase
+from pydantic import BaseModel
+from datetime import datetime
 
+
+class SubscriptionResponse(ORMBase):
+    user_id: int
+    plan_id: int | None
+    status: str
+    price_amount: float | None
+    price_interval: str
+    stripe_subscription_id: str
+    stripe_customer_id: str
+    current_period_start: datetime | None
+    current_period_end: datetime | None
