033a328094dda42ad126819967df5a35fef7839c
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 17:54:22 +0530

    Update team.py

--- Backend/app/schemas/team.py
+++ Backend/app/schemas/team.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+from .base import ORMBase
 
+
+class TeamBase(BaseModel):
+    name: str
+    is_active: bool = True
+    credits: float | None = 0
+    stripe_customer_id: str | None = None
+
+
+class TeamCreate(BaseModel):
+    name: str
+
+
+class TeamResponse(ORMBase, TeamBase):
+    owner_id: int
