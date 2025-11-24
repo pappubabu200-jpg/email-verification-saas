851885abd54192205bd95f026682b6a36ce8a2b7
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 17:55:12 +0530

    Update team_member.py

--- Backend/app/schemas/team_member.py
+++ Backend/app/schemas/team_member.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel
+from .base import ORMBase
 
+
+class TeamMemberBase(BaseModel):
+    role: str
+    active: bool = True
+
+
+class TeamMemberResponse(ORMBase, TeamMemberBase):
+    user_id: int
+    team_id: int
