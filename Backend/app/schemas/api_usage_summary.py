5fb3d073b5211714aeeb140a54ec003d929bfa7c
Pappu Babu <pappubabu200@gmail.com>
2025-11-24 18:00:02 +0530

    Update api_usage_summary.py

--- Backend/app/schemas/api_usage_summary.py
+++ Backend/app/schemas/api_usage_summary.py
@@ -1,1 +1,13 @@
+from .base import ORMBase
+from pydantic import BaseModel
+from datetime import date
 
+
+class ApiUsageSummaryResponse(ORMBase):
+    user_id: int
+    team_id: int | None
+    date: date
+    total_requests: int
+    total_verifications: int
+    total_bulk_processed: int
+    cost: float
