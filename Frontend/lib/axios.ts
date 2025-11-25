8bf397149b55639f742bd00e5052ac40bf831783
Pappu Babu <pappubabu200@gmail.com>
2025-11-25 12:48:05 +0530

    Update axios.ts

--- Frontend/lib/axios.ts
+++ Frontend/lib/axios.ts
@@ -1,1 +1,8 @@
+import axios from "axios";
 
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000/api/v1",
+  withCredentials: true,
+});
+
+export default instance;
