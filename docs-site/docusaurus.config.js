abd783700467c4c973fac20f382bf4d0f38169f6
Pappu Babu <pappubabu200@gmail.com>
2025-11-29 10:27:25 +0530

    Update docusaurus.config.js

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -1,1 +1,26 @@
-
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'ZeroVerify Docs',
+  tagline: 'Email verification & decision-maker platform docs',
+  url: 'https://<YOUR-GITHUB-USER>.github.io',
+  baseUrl: '/<YOUR-REPO-NAME>/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: '/img/favicon.ico',
+  organizationName: '<YOUR-GITHUB-USER>',
+  projectName: '<YOUR-REPO-NAME>',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/', // serve docs at root
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
